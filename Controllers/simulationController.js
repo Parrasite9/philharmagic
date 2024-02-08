0a02a6b929731f1b9d1cfbd655b37cafc5348128
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-02-08 14:21:58 -0600

    simulationController with basic functionality.

---
+++ Controllers/simulationController.js
@@ -0,0 +1,15 @@
+const SimulationEvent = require('../Models/SimulationEvent')
+
+exports.startSimulation = async (req, res) => {
+    // LOGIC TO  START SIMULATION 
+    res.send('Simulation started');
+}
+
+exports.pauseSimulation = async (req, res) => {
+    // LOGIC TO PAUSE SIMULATION 
+    res.send('Simulation paused')
+}
+exports.resetSimulation = async (req, res) => {
+    // LOGIC TO PAUSE SIMULATION 
+    res.send('Simulation reset')
+}
