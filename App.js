84a75809ea4931b3a2c3e7692b734ef41cdd10a8
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-02-08 18:27:00 -0600

    Added comments to files

--- App.js
+++ App.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const connectDB = require('./MongoDB/mongodb')
+
+// Load environment variables from a .env file into process.env
+require('dotenv').config()
+
+// Call the function to connect to the MongoDB database
+connectDB()
+
+
 const app = express()
+
+// Middleware to parse JSON bodies in requests
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hell, World!')
--- MongoDB/mongodb.js
+++ MongoDB/mongodb.js
@@ -1,11 +1,15 @@
+// IMPORT MONGODB 
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
+        // Attempt to connect to the MongoDB database using the connection URI from environment variables
+        // and set some options for the connection to use the new URL parser and unified topology engine
         await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
+        // IF IT CONNECTS PROPERLY 
         console.log('MongoDB connected...');
     } catch (err) {
         console.error('Failed to connect to MongoDB', err);
